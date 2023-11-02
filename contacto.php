ee88e42965e0c9441dc2dd34a49671e914c46a8a
luluserr <147454196+luluserr@users.noreply.github.com>
2023-11-01 21:05:51 -0300

    Add files via upload

---
+++ contacto.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+Tu nombre es: <?php echo $_POST["nombre"]; ?>
+Tu e-mail es: <?php echo $_POST["email"]; ?>
+Tu mensaje fue: <?php echo $_POST["mensaje"]; ?>
+    </body>
+</html>
